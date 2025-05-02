b9881565adc3ce70860ff229ee7c63ce4cd295e3
Nikita Lee <mrmitik.2003@gmail.com>
2025-05-02 10:02:08 +0300

    edit model

--- src/models/messages.py
+++ src/models/messages.py
@@ -10,7 +10,7 @@ class MessagesOrm(Base):
     __tablename__ = "messages"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    sender_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    sender_name: Mapped[str] = mapped_column(String(50), nullable=False)
     text: Mapped[str] = mapped_column(String(500), nullable=False)
     user_message_count: Mapped[int] = mapped_column(nullable=False)
     created_at: Mapped[datetime]
